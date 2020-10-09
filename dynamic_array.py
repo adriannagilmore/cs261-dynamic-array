0aed235af887b11c2d93de8ec3d937d44ab5ea7a
Adrianna Gilmore <a_inman33@yahoo.com>
2020-10-08 23:07:50 -0700

    passed 4 tests

--- dynamic_array.py
+++ dynamic_array.py
@@ -2,5 +2,21 @@
 # Your implementation should pass the tests in test_dynamic_array.py.
 # YOUR NAME
 
-class FixMe:
-    pass
+class DynamicArray:
+    def __init__(self):
+        self.capacity = 10
+        self.length = 0
+        
+
+    def is_empty(self):
+        return True
+    
+    def __len__(self):
+        return self.length
+
+    
+
+
+
+
+
